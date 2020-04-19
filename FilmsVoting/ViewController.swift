188fad7e95b87f8fbde14b634e0a2ef4e87ec1a5
Vladislav <vlad@grokhotov.ru>
2020-04-19 22:07:00 +0300

    initial

---
+++ FilmsVoting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FilmsVoting
+//
+//  Created by Владислав on 19.04.2020.
+//  Copyright © 2020 Vladislav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
